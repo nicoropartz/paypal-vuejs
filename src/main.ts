5a273881627723731f060af0e06f5294b6e61693
nropartz <n.ropartz@groupeonepoint.com>
2023-07-04 14:35:48 +0200

    commit 1 feature

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import locale from 'element-plus/lib/locale/lang/fr';
-import Element from 'element-plus'
-import './assets/sass/customization.scss'
-import './assets/sass/element-variables.scss';
-import './assets/sass/customization.scss';
-import './assets/sass/paper-dashboard.scss';
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import locale from "element-plus/lib/locale/lang/fr";
+import Element from "element-plus";
+import "./assets/sass/customization.scss";
+import "./assets/sass/element-variables.scss";
+import "./assets/sass/customization.scss";
+import "./assets/sass/paper-dashboard.scss";
 
-createApp(App)
-.use(Element, { locale })
-.mount('#app')
+//test
+createApp(App).use(Element, { locale }).mount("#app");
